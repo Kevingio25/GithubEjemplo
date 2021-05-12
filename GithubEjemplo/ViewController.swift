3dc5c52ac7dbe9cbba26c0184f501abbcf5b4a73
Kevin Solano <kevingiovani25@gmail.com>
2021-05-11 21:44:26 -0500

    Se agregó una etiqueta y se le cambió el texto en el ViewController

--- GithubEjemplo/ViewController.swift
+++ GithubEjemplo/ViewController.swift
@@ -9,11 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labelTitulo: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        labelTitulo.text = Hey GitHub
     }
-
-
+    
+    
 }
 
